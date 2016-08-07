a96ae26e841fc1ab9fe5a2e50e7701cbf65a353c
zhangtiantian10 <975571814@qq.com>
2016-08-07 14:45:18 +0800

    write AddList component and can add list

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,41 @@
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lists: []
+        }
+    }
+    
+    addList(value) {
+        const lists = this.state.lists;
+        lists.push({value, static: true});
+
+        this.setState({lists});
+        console.log(lists);
+    }
+
     render() {
         return <div>
-            <AddList />
+            <AddList onAdd={this.addList.bind(this)}/>
             <ShowLists />
         </div>
     }
 }
 
 class AddList extends React.Component {
+    enterChange(e) {
+        if(e.keyCode === 13) {
+            let value = e.target.value;
+            if (!value) return false;
+
+            this.props.onAdd(document.getElementById('input').value);
+            document.getElementById('input').value = '';
+        }
+    }
+
     render() {
         return <div>
-            AddList
+            <input type="text" id="input" onKeyDown={this.enterChange.bind(this)}/>
         </div>
     }
 }
