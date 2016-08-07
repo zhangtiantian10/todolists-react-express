e06f0d1e6e457a6dd5dc82b9717dc62e9558e841
zhangtiantian10 <975571814@qq.com>
2016-08-07 17:14:22 +0800

    add footer

--- server.js
+++ server.js
@@ -9,26 +9,44 @@ app.use(bodyParser.urlencoded());
 
 app.use(express.static(__dirname + '/public'));
 
-let lists = [{value: '123', static: true}, {value:'456', static:true}]
+let i = 2;
+
+let lists = [{value: '123', static: true, index: 0}, {value:'456', static:true, index: 1}]
 
 app.get('/app/todolists', (req, res) => {
     res.json(lists);
 });
 
 app.post('/app/todolist', (req, res) => {
-    lists.push(req.body.list);
+    lists.push({value:req.body.value,static: true, index: i});
+    i++;
 
     res.json(lists);
 });
 
 app.delete('/app/todolist', (req,res) => {
-    lists.splice(req.body.index, 1);
+    let index;
+    for(let j = 0; j < lists.length ; j++) {
+        if(lists[j].index === req.body.index){
+            index = j;
+            break;
+        }
+    }
+
+    lists.splice(index, 1);
 
     res.json(lists);
 });
 
 app.put('/app/todolist', (req, res) => {
-    lists[req.body.index].static = !lists[req.body.index].static;
+    let index;
+    for(let j = 0; j < lists.length ; j++) {
+        if(lists[j].index === parseInt(req.body.index)){
+            index = j;
+            break;
+        }
+    }
+    lists[index].static = !lists[index].static;
 
     res.json(lists);
 })
