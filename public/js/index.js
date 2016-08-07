bb7ea85f1e79aa475d81ffb286b05a9891cf66b3
zhangtiantian10 <975571814@qq.com>
2016-08-07 15:11:39 +0800

    can delete list

--- public/js/index.js
+++ public/js/index.js
@@ -5,10 +5,24 @@ class App extends React.Component {
             lists: []
         }
     }
-    
+
     addList(value) {
         const lists = this.state.lists;
         lists.push({value, static: true});
+
+        this.setState({lists});
+    }
+
+    deleteList(index) {
+        const lists = this.state.lists;
+        lists.splice(index, 1);
+
+        this.setState({lists});
+    }
+
+    modifyStatic(index) {
+        const lists = this.state.lists;
+        lists[index].static = !lists[index].static;
 
         this.setState({lists});
         console.log(lists);
@@ -17,8 +31,8 @@ class App extends React.Component {
     render() {
         return <div>
             <AddList onAdd={this.addList.bind(this)}/>
-            <ShowLists />
-        </div>
+            <ShowLists lists={this.state.lists} onDelete={this.deleteList.bind(this)} onChangeStatic={this.modifyStatic.bind(this)}/>
+        </div>;
     }
 }
 
@@ -41,10 +55,48 @@ class AddList extends React.Component {
 }
 
 class ShowLists extends React.Component {
+    deleteList(index) {
+        this.props.onDelete(index);
+    }
+
     render() {
+        const lists = this.props.lists.map((list, index) => {
+            return <div key={index}>
+                <li>
+                    <CheckBox onChangeStatic={this.props.onChangeStatic} index={index}/>
+                    {list.value}
+                    <button onClick={this.deleteList.bind(this, index)}>删除</button>
+                </li>
+            </div>;
+        })
+
         return <div>
-            ShowList
+            <ul>
+            {lists}
+            </ul>
         </div>
+    }
+}
+
+class CheckBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checked: false
+        }
+    }
+
+    changeStatic() {
+        this.setState({
+            checked: !this.state.checked
+        });
+
+        this.props.onChangeStatic(this.props.index);
+    }
+
+
+    render() {
+        return <input type="checkbox" onChange={this.changeStatic.bind(this)} checked={this.state.checked}/>
     }
 }
 
